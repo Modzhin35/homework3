a180f763d5c943459c873ff45f1ea22d3c17f062
modzhin <modzhds@gmail.com>
2022-12-25 20:03:30 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое число N, кроме нуля: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 0) { Console.Write("Ошибка. Введите любое число, кроме нуля."); }
+else
+{
+    if (num < 0) { num = -num; }
+    int i = 1;
+    while (i <= num)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+        i++;
+    }
+}
